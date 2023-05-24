8887537a4b6f6c5149b73f2b83a2622e42a2aea7
Liam Fernandez <liamdfernandez@gmail.com>
2023-05-24 12:50:34 -0700

    have got a draft of the AboutUs page up

---
+++ src/strings.ts
@@ -0,0 +1,14 @@
+export const aboutMap = {
+    "Rags": {
+        "anime": "bars-7",
+        "description": "In the calmness of twilight, the sun dipped below the horizon, painting the sky with hues of pink and gold. A gentle breeze whispered through the trees, carrying with it the promise of a tranquil evening and the sweet scent of blooming flowers."
+    },
+    "Lex": {
+        "anime": "bars-5",
+        "description": "In the calmness of twilight, the sun dipped below the horizon, painting the sky with hues of pink and gold. A gentle breeze whispered through the trees, carrying with it the promise of a tranquil evening and the sweet scent of blooming flowers."
+    },
+    "Morgen": {
+        "anime": "bars-6",
+        "description": "In the calmness of twilight, the sun dipped below the horizon, painting the sky with hues of pink and gold. A gentle breeze whispered through the trees, carrying with it the promise of a tranquil evening and the sweet scent of blooming flowers."
+    }
+}
