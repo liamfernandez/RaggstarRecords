a006f9d79c2da8a6bc4df58dd14cbd8d3afc8279
Liam Fernandez <liamdfernandez@gmail.com>
2024-05-31 16:56:10 -0400

    Adding meta tags

--- src/routes/(landing)/+layout.svelte
+++ src/routes/(landing)/+layout.svelte
@@ -49,6 +49,38 @@
 <svelte:head>
 	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" />
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" />
+	<!-- Meta Pixel Code -->
+	<!-- Meta Pixel Code -->
+	<script>
+		!(function (f, b, e, v, n, t, s) {
+			if (f.fbq) return;
+			n = f.fbq = function () {
+				n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+			};
+			if (!f._fbq) f._fbq = n;
+			n.push = n;
+			n.loaded = !0;
+			n.version = '2.0';
+			n.queue = [];
+			t = b.createElement(e);
+			t.async = !0;
+			t.src = v;
+			s = b.getElementsByTagName(e)[0];
+			s.parentNode.insertBefore(t, s);
+		})(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '1502090967046087');
+		fbq('track', 'PageView');
+	</script>
+	<noscript>
+		<img
+			height="1"
+			width="1"
+			alt="evil fb pixel"
+			style="display:none"
+			src="https://www.facebook.com/tr?id=1502090967046087&ev=PageView&noscript=1"
+		/>
+	</noscript>
+	<!-- End Meta Pixel Code -->
 </svelte:head>
 <div class="drawer md:block">
 	<input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
